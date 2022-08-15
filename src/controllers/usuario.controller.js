74acf5d8960e89a1889594f6a1e98dc164ee1f7c
OmgCopito95 <july1995bolt@gmail.com>
2022-08-15 16:31:41 -0300

    endpoint subir y descargar archivos

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -27,8 +27,8 @@ module.exports = {
                 where: {
                     id: req.params.idUsuario
                 }
-            })            
-            if(!user) return next(errors.UsuarioInexistente)
+            })
+            if (!user) return next(errors.UsuarioInexistente)
 
             res.json({
                 success: true,
@@ -69,6 +69,78 @@ module.exports = {
         } catch (err) {
             console.log(err)
         }
+    },
+
+    subirArchivo: async (req, res, next) => {
+        try {
+
+            //verifico si existe el usuario
+            const usuario = await models.usuario.findOne({
+                where: {
+                    id: req.body.usuarioId
+                }
+            })
+            if (!usuario) return next(errors.UsuarioInexistente)
+
+
+            // busco el archivo del usuario
+            const ar = await models.archivo_usuario.findOne({
+                where: {
+                    usuarioId: req.body.usuarioId,
+                    nombre: req.body.nombre
+                }
+            })
+            if (!ar) { // si el archivo no existe, lo creo
+
+                const archivo = await models.archivo_usuario.create({
+                    nombre: req.body.nombre, //nombre para identificar el archivo por si un usuario tiene varios archivos
+                    file: req.file ? req.file.filename : null, //en el campo file se guarda el nombre del archivo
+                    original_name: req.file ? req.file.originalname : null, //en el campo original_name se guarda el nombre original del archivo
+                    usuarioId: req.body.usuarioId
+                })
+
+            }
+
+
+            res.json({
+                success: true,
+                data: {
+                    message: "archivo cargado"
+                }
+            })
+
+        } catch (err) {
+            return next(err)
+        }
+    },
+
+    descargarArchivo: async (req, res, next) => {
+        try {
+
+            // verifico si existe el usuario
+            const usuario = await models.usuario.findOne({
+                where: {
+                    id: req.body.usuarioId
+                }
+            })
+            if (!usuario) return next(errors.UsuarioInexistente)
+
+            // verifico si existe el archivo
+            const archivo = await models.archivo_usuario.findOne({
+                where: {
+                    usuarioId: req.body.usuarioId,
+                    nombre: req.body.nombre
+                }
+            })
+            if (!archivo) return next(errors.ArchivoInexistente)
+
+
+            res.download('uploads/archivos-usuarios/' + archivo.file, archivo.original_name) //descarga el archivo
+
+        } catch (err) {
+            return next(err)
+        }
     }
+
 
 }
